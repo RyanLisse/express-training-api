736a4b4e6cf4b1620d741004569dfcdf8073c7db
RyanLisse <57917217+RyanLisse@users.noreply.github.com>
2023-04-27 23:47:29 +0200

    Implented the post route

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+const path = require("path");
 
-const PORT = process.env.PORT || 4000
+const app = express();
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello from Nerdbord!')
-})
+const PORT = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from Nerdbord!");
+});
+
+app.post("/trains", (req, res) => {
+  const createTrainPayload = req.body;
+  const trainsFilePath = path.join(__dirname, "data", "trains.json");
+  const trains = JSON.parse(fs.readFileSync(trainsFilePath));
+  trains.push(createTrainPayload);
+  fs.writeFileSync(trainsFilePath, JSON.stringify(trains));
+  res.send("Train added successfully");
+});
 
 app.listen(PORT, () => {
-    console.log('Server listening on port 3000')
-})
+  console.log(`Server listening on port ${PORT}`);
+});
